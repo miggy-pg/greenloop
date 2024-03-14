4fd61b87e820de610092eeb4c662d69abb5e3205
miggy-pg <meelan1323@gmail.com>
2024-03-15 00:57:54 +0800

    wip: fix no cors backend

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const app = express();
 // const cors = require("cors");
 const prodOrigin = [process.env.ORIGIN_1, process.env.ORIGIN_2];
 const devOrigin = ["http://localhost:5173"];
@@ -26,7 +27,6 @@ dotenv.config();
 // Connect DB
 require("./db/connection");
 
-const app = express();
 app.use(
   express.json({
     limit: "50mb",
