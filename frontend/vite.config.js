80260d33a29f6b92c24693beb393f3c6d47a7f29
miggy-pg <meelan1323@gmail.com>
2024-03-14 22:02:38 +0800

    chore: deployment fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import InjectCSS from "@itsy/vite-css-inject";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: { global: "globalThis" },
+  plugins: [react(), InjectCSS()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@tests": path.resolve(__dirname, "./tests"),
+    },
+  },
+  // define: { global: "globalThis" },
 });
