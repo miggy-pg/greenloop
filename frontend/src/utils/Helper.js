f347433c147ab4158bebdfa6103edc32e64c6d2f
miggy-pg <meelan1323@gmail.com>
2024-03-22 03:17:39 +0800

    wip: fix socketio port

--- frontend/src/utils/Helper.js
+++ frontend/src/utils/Helper.js
@@ -11,7 +11,7 @@ export const getEndpoint = envRoute(
 
 export const socketPort =
   import.meta.env.VITE_ENV === "production"
-    ? process.env.VITE_PROD_BASE_URL + ":8080"
+    ? import.meta.env.VITE_PROD_BASE_URL + ":8080"
     : "http://localhost:8080";
 
 export const devPrint = (message, ...rest) => {
