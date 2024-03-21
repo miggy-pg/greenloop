50024e21355c2eb715a96eff8ed9a6c9d6268698
miggy-pg <meelan1323@gmail.com>
2024-03-22 03:11:12 +0800

    wip: fix vercel

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -2,6 +2,8 @@ import axios from "redaxios";
 import { getEndpoint } from "../utils/Helper";
 
 export const signInUser = async (userData) => {
+  console.log("userData: ", userData);
+  console.log("getEndPoint: ", getEndpoint);
   return await axios.post(`${getEndpoint}/api/sign-in`, userData);
 };
 
