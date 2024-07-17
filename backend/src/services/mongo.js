638686b8c715bebf60ea255ebea3e531285007bc
miggy-pg <meelan1323@gmail.com>
2024-07-18 06:44:41 +0800

    chore: update env template

--- backend/src/services/mongo.js
+++ backend/src/services/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function mongoConnect() {
-  await mongoose.connect(process.env.MONGO_URL, {
+  await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -2,19 +2,9 @@ import axios from "redaxios";
 import { getEndpoint } from "../utils/Helper";
 
 export const signInUser = async (userData) => {
-  console.log("userData: ", userData);
-  console.log(
-    "signIn: ",
-    await axios.post(`${getEndpoint}/v1/sign-in`, userData)
-  );
   return await axios.post(`${getEndpoint}/v1/sign-in`, userData);
 };
 
 export const signUpUser = async (userData) => {
-  console.log("signUpUser: ", userData);
-  console.log(
-    "signUp: ",
-    await axios.post(`${getEndpoint}/v1/sign-up`, userData)
-  );
   return await axios.post(`${getEndpoint}/v1/sign-up`, userData);
 };
